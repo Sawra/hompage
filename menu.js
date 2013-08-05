321e30e005245d62e57771073bafd6f33dd86337
Sara <systemadmin@utnarm.utn.se>
2013-08-05 15:43:33 +0200

    Revert "JS for menu"
    This reverts commit 6941443c65d702f4679d4a65b7fe77b8ddedfc77.

--- menu.js
+++ menu.js
@@ -1,64 +1,25 @@
 
 function setUp() {
-	
-	var buttons = document.getElementsByClassName("button");
-	var shape = document.getElementsByClassName("shape");
-
-	
-	for(var i = 0; i < buttons.length; i++) {
-		buttons[i].addEventListener("click", buttonClick, false);
-		
-	} 
-	
+	var links = document.getElementsByClassName("menuItem");
+	for(var i = 0; i < links.length; i++) {
+		links[i].addEventListener("mouseover", menuItemDown, false);
+		links[i].addEventListener("mouseout", menuItemSmall, false);
+	}
 		
 }
 
-function blur(e) {
-	alert("funker");
+function menuItemDown(e) {
+	console.log(e.target.id);
+	e.target.style.width ="110px";
+	e.target.style.height ="110px";
+	
+	
 }
-
-function buttonClick(e) {
+function menuItemSmall(e) {
+	console.log(e.target.id);
+	e.target.style.width ="100px";
+	e.target.style.height ="100px";
 	
-	console.log(e.target);
-	var button = e.target.id;
-	var first = document.getElementById("first");
-	if (button == "about") {
-		first.style.marginTop ="-1300px";
-		
-	}
-
-	else if (button == "contact") {
-		first.style.marginTop ="-2350px";
-	}
-
-	else if (button == "mystuff") {
-		first.style.marginTop ="-3400px";
-	}
-	
-	
-
-	
-
-}
-
-function buttonClickOut(e) {
-	
-	console.log(e.target.parentElement.parentElement);
-	var shape = e.target.parentElement.parentElement;
-	var img =shape.firstElementChild;
-var button = e.target;
-	var details = shape.getElementsByClassName("details")[0];
-	var header = details.getElementsByClassName("heading")[0];
-	img.style.height ="150px";
-	img.style.width ="145px";
-	shape.style.top ="0px"
-	shape.style.right = "0px";
-	details.style.width ="136px";
-	details.style.marginLeft ="0";
-	details.style.marginTop ="0";
-	header.style.fontSize ="15px";
-	button.removeEventListener("click", buttonClickOut, false);
-	button.addEventListener("click", buttonClick, false);
 	
 }
 
